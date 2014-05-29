7337d8ac2d681c6904270cf327eb3be044657e0e
Devendra <devendra@udgama.com>
2014-05-29 15:03:57 +0530

    saving changes

--- android/examples/PubnubSubscribe/src/com/pubnub/examples/pubnubsubscribe/PubnubActivity.java
+++ android/examples/PubnubSubscribe/src/com/pubnub/examples/pubnubsubscribe/PubnubActivity.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 import com.pubnub.examples.pubnubsubscribe.R;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,46 +22,82 @@ import android.widget.ListView;
 
 public class PubnubActivity extends Activity {
 	
+    public static final int NOTIFICATION_ID = 1;
+	private static final String TAG = "PubnubGcm";
+    private NotificationManager mNotificationManager;
 	public ArrayList<String> list = new ArrayList<String>();
 	public ArrayAdapter adapter;
+	
+	boolean sendNotification = true;
+	
+    private void sendNotification(String msg) {
+        mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, PubnubActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+        .setSmallIcon(R.drawable.icon)
+        .setContentTitle("GCM Notification")
+        .setStyle(new NotificationCompat.BigTextStyle()
+        .bigText(msg))
+        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+	
+	public void notifyUser(String message) {
+		if (!sendNotification) {
+			list.add(0, message);
+			adapter.notifyDataSetChanged();
+		} else {
+			sendNotification(message);
+		}
+	}
+	
     BroadcastReceiver receiver = new BroadcastReceiver() {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			list.add(0, intent.getStringExtra("data"));
-			adapter.notifyDataSetChanged();
+			notifyUser(intent.getStringExtra("data"));
 		}
     };
 	
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-
         setContentView(R.layout.activity_pubnub);
-        
         
         ListView listview = (ListView) findViewById(R.id.message_list);
 
-        adapter = new ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, list);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
 
         listview.setAdapter(adapter);
         
-        final IntentFilter myFilter = new
-
-        		IntentFilter("android.intent.action.MAIN");
-
-        		registerReceiver(receiver, myFilter);
-
-        
-        // In new versions of Android, the service may not be activated unless an
-        // associated activity is run at least once. This empty activity serves
-        // that purpose
+        IntentFilter myFilter = new IntentFilter("android.intent.action.MAIN");
+        registerReceiver(receiver, myFilter);
 
         Intent serviceIntent = new Intent(this, PubnubService.class);
         startService(serviceIntent);
 
         Log.i("HelloWorldActivity", "PubNub Activity Started!");
 
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	sendNotification = false;
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	sendNotification = true;
     }
 }
--- android/examples/PubnubSubscribe/src/com/pubnub/examples/pubnubsubscribe/PubnubService.java
+++ android/examples/PubnubSubscribe/src/com/pubnub/examples/pubnubsubscribe/PubnubService.java
@@ -1,83 +1,122 @@
 package com.pubnub.examples.pubnubsubscribe;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
 
 import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
+import com.pubnub.api.PubnubError;
 import com.pubnub.api.PubnubException;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 public class PubnubService extends Service {
 
+	private static final String TAG = "PubnubGcm";
+
     public static final String ACTION_FROM_SERVICE = null;
-	String channel = "hello_world1";
-    Pubnub pubnub = new Pubnub("demo", "demo", false);
+	
+    String device_channel = "Android-1";
+	String public_channel = "public";
+	String auth_key = "Android-AuthToken";
+	String uuid = "Android-user1";
+	
+	String publish_key = "pam";
+	String subscribe_key = "pam";
+	
+    Pubnub pubnub;
     PowerManager.WakeLock wl = null;
 
-    private final Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            String pnMsg = msg.obj.toString();
-            
-            final Toast toast = Toast.makeText(getApplicationContext(), pnMsg, Toast.LENGTH_SHORT);
-            toast.show();
-
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    toast.cancel();
-                }
-            }, 200);
-
-        }
-    };
-
     private void notifyUser(Object message) {
-
-        Message msg = handler.obtainMessage();
-
     	Intent intent = new Intent("android.intent.action.MAIN");
         intent.putExtra("data", message.toString());
         sendBroadcast(intent);
-        /*
-        try {
-            final String obj = (String) message;
-            msg.obj = obj;
-            handler.sendMessage(msg);
-            Log.i("Received msg : ", obj.toString());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        */
     }
 
     public void onCreate() {
         super.onCreate();
-        //Toast.makeText(this, "PubnubService created...", Toast.LENGTH_LONG).show();
+        pubnub = new Pubnub(publish_key, subscribe_key, false);
+        pubnub.setUUID(uuid);
+        pubnub.setAuthKey(auth_key);
+        
+        pubnub.history(public_channel, 3, new Callback(){
+        	@Override
+        	public void successCallback(String channel, Object message) {
+        		Log.i("PUBNUB", message.toString());
+        		JSONArray jsa ;
+				try {
+					jsa = (JSONArray) ((JSONArray) message).get(0);
+	        		 for (int i=0; i < 3; i++) {
+	 					notifyUser(jsa.get(0));
+	         		}
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+        		
+
+        		
+        	}
+        	@Override
+        	public void errorCallback(String channel, PubnubError error) {
+        		
+        	}
+        });
+        
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "SubscribeAtBoot");
         if (wl != null) {
             wl.acquire();
             Log.i("PUBNUB", "Partial Wake Lock : " + wl.isHeld());
-            //Toast.makeText(this, "Partial Wake Lock : " + wl.isHeld(), Toast.LENGTH_LONG).show();
         }
 
         Log.i("PUBNUB", "PubnubService created...");
+        
+        
+        
         try {
-            pubnub.subscribe(new String[] {channel}, new Callback() {
+            pubnub.subscribe(public_channel, new Callback() {
                 public void connectCallback(String channel) {
-                    //notifyUser("CONNECT on channel:" + channel);
+                    
+                	try {
+						pubnub.subscribe(device_channel, new Callback() {
+			                public void connectCallback(String channel) {
+			                    //notifyUser("CONNECT on channel:" + channel);
+			                }
+			                public void disconnectCallback(String channel) {
+			                    //notifyUser("DISCONNECT on channel:" + channel);
+			                }
+			                public void reconnectCallback(String channel) {
+			                    //notifyUser("RECONNECT on channel:" + channel);
+			                }
+			                @Override
+			                public void successCallback(String channel, Object message) {
+			                    notifyUser(message.toString());
+			                }
+			                @Override
+			                public void errorCallback(String channel, Object message) {
+			                    notifyUser(message.toString());
+			                }
+						});
+					} catch (PubnubException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                	
                 }
                 public void disconnectCallback(String channel) {
                     //notifyUser("DISCONNECT on channel:" + channel);
@@ -97,7 +136,10 @@ public class PubnubService extends Service {
         } catch (PubnubException e) {
 
         }
+           
     }
+    
+
 
     @Override
     public void onDestroy() {

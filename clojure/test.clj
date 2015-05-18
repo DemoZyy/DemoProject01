d98b001cb865a191a5d3189594725e51e031633f
Devendra <devendra@udgama.com>
2015-05-18 23:47:43 +0530

    changing channel

--- clojure/test.clj
+++ clojure/test.clj
@@ -2,7 +2,7 @@
 
 
 (defn test-pubnub []
-   (.subscribe (new com.pubnub.api.Pubnub "demo" "demo") "abcd1234" (proxy [com.pubnub.api.Callback] [] 
+   (.subscribe (new com.pubnub.api.Pubnub "demo" "demo") "a" (proxy [com.pubnub.api.Callback] [] 
    		(successCallback ([channel message] (println (str message " " channel)))
    		([channel message timetoken] ))
    		(connectCallback [channel message] (println (str "Connected to " channel)))

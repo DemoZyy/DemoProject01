67b1ecbdb27831adb7d3cb54576173e8f3068d4b
Devendra <devendra@udgama.com>
2015-05-18 22:22:16 +0530

    adding clojure example

---
+++ clojure/test.clj
@@ -0,0 +1,11 @@
+
+
+
+(defn test-pubnub []
+   (.subscribe (new com.pubnub.api.Pubnub "demo" "demo") "abcd1234" (proxy [com.pubnub.api.Callback] [] 
+   		(successCallback ([channel message] (println (str message " " channel)))
+   		([channel message timetoken] ))
+   		(connectCallback [channel message] (println (str "Connected to " channel)))
+   		)))
+(test-pubnub)
+

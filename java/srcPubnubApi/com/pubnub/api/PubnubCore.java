0c827e1d6ed540e4631697289d24cf42ada23183
Devendra <devendra@udgama.com>
2015-12-18 00:54:19 +0530

    adding fix for publishing json literals

--- java/srcPubnubApi/com/pubnub/api/PubnubCore.java
+++ java/srcPubnubApi/com/pubnub/api/PubnubCore.java
@@ -232,6 +232,7 @@ abstract class PubnubCore implements PubnubInterface {
             }
         } else {
             if (message instanceof String) {
+            	msgStr = msgStr.replaceAll("\"", "\\\\\"");
                 msgStr = "\"" + msgStr + "\"";
             }
         }
